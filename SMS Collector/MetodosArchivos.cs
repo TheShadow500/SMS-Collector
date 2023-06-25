af06bc1130e0194efd2e7046849cf2e3de99b55b
TheShadow <damorescorzo@gmail.com>
2023-06-25 18:50:44 +0200

    version 5.22 - Final (24/06/23) ----------------------------------------------
    Errores Conocidos
    - El menú de visualización muestra un parpadeo puntual al cambiar de número de teléfono que no afecta al funcionamiento
    
    Errores Corregidos
    - Ninguno
    
    Mejoras
    - Optimización del código
    - Se optimiza la Biblioteca de Métodos para Archivos
    
    Próximas Mejoras Programadas
    - Terminar de implementar en toda la aplicacion la Biblioteca de Métodos para Archivos
    - Terminar la corrección de las nomenclaturas

--- SMS Collector/MetodosArchivos.cs
+++ SMS Collector/MetodosArchivos.cs
@@ -10,15 +10,54 @@ namespace SMS_Collector
 {
     class MetodosArchivos
     {
+        const string archivo_configuracion = "Config.cfg";
+        const string archivo_numeros = "Numeros.dat";
+        const string archivo_intentos = "Intentos.dat";
+        const string archivo_datos = "Datos.dat";
+
         BinaryFormatter serie = new BinaryFormatter();
         FileStream flujo;
         FileStream flujo2;
+        Configuracion datos;
+
+        public bool ComprobarArchivo(int opcion)
+        {
+            switch(opcion)
+            {
+                case 1:
+                    if(File.Exists(archivo_configuracion))
+                    {
+                        return true;
+                    }
+                    break;
+                case 2:
+                    if(File.Exists(archivo_numeros))
+                    {
+                        return true;
+                    }
+                    break;
+                case 3:
+                    if(File.Exists(archivo_intentos))
+                    {
+                        return true;
+                    }
+                    break;
+                case 4:
+                    if(File.Exists(archivo_datos))
+                    {
+                        return true;
+                    }
+                    break;
+                default:
+                    return false;
+            }
+
+            return false;
+        }
 
         public Configuracion CargarUsuario()
         {
-            Configuracion datos;
-
-            if (File.Exists("Config.cfg"))
+            if (ComprobarArchivo(1))
             {
                 flujo = new FileStream("Config.cfg", FileMode.Open, FileAccess.Read);
                 datos = (Configuracion)serie.Deserialize(flujo);
@@ -27,8 +66,9 @@ namespace SMS_Collector
             else
             {
                 datos = new Configuracion("Admin", 12345);
-                MessageBox.Show("No hay ningún usuario creado.\nSe han aplicado los valores por defecto:\n\nUsuario: Admin\nContraseña: 12345", "ATENCIÓN", MessageBoxButtons.OK);
-                MessageBox.Show("Para mayor seguridad, asegúrese de configurar el nombre de usuario y la contraseña desde Archivo -> Preferencias", "ATENCIÓN", MessageBoxButtons.OK);
+                MessageBox.Show("No existe el archivo Config.cfg", "ATENCIÓN", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                MessageBox.Show("No hay ningún usuario creado.\nSe han aplicado los valores por defecto:\n\nUsuario: Admin\nContraseña: 12345", "ATENCIÓN", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("Para mayor seguridad, asegúrese de configurar el nombre de usuario y la contraseña desde Archivo -> Preferencias", "ATENCIÓN", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
 
             MessageBox.Show("Cargado el perfil: " + datos.DevolverUsuario, "Información", MessageBoxButtons.OK);
@@ -38,36 +78,37 @@ namespace SMS_Collector
 
         public void ModificarContrasena(Configuracion datos)
         {
-            flujo = new FileStream("Config.cfg", FileMode.Create, FileAccess.Write);
+            flujo = new FileStream(archivo_configuracion, FileMode.Create, FileAccess.Write);
             serie.Serialize(flujo, datos);
             flujo.Close();
 
-            MessageBox.Show("Modificado con éxito.\nDebe reiniciar el programa para que los cambios tengan efecto", "Información", MessageBoxButtons.OK);
+            MessageBox.Show("Modificado con éxito.\nSe reiniciará el programa para que los cambios tengan efecto", "ATENCIÓN", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            Application.Restart();
         }
 
         public void AnadirContacto(Persona persona)
         {
-            if(File.Exists("Numeros.dat"))
+            if(ComprobarArchivo(2))
             {
-                flujo = new FileStream("Numeros.dat", FileMode.Append, FileAccess.Write);
+                flujo = new FileStream(archivo_numeros, FileMode.Append, FileAccess.Write);
             }
             else
             {
-                flujo = new FileStream("Numeros.dat", FileMode.Create, FileAccess.Write);
+                flujo = new FileStream(archivo_numeros, FileMode.Create, FileAccess.Write);
             }
 
             serie.Serialize(flujo, persona);
             flujo.Close();
-            MessageBox.Show("Perfil guardado con éxito", "Información", MessageBoxButtons.OK);
+            MessageBox.Show("Perfil guardado con éxito", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         public ArrayList ConsultarIntentos()
         {
             ArrayList intentos = new ArrayList();
 
-            if (File.Exists("Intentos.dat"))
+            if (ComprobarArchivo(3))
             {
-                flujo = new FileStream("Intentos.dat", FileMode.Open, FileAccess.Read);
+                flujo = new FileStream(archivo_intentos, FileMode.Open, FileAccess.Read);
 
                 try
                 {
@@ -85,7 +126,7 @@ namespace SMS_Collector
             }
             else
             {
-                MessageBox.Show("No hay ningún registro", "Información", MessageBoxButtons.OK);
+                MessageBox.Show("No hay ningún registro", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
 
             return intentos;
@@ -97,13 +138,13 @@ namespace SMS_Collector
 
             error = Convert.ToString("Usuario: " + usuario.DevolverUsuario + " - " + DateTime.Now + " - Contraseña: " + contrasena);
 
-            if (File.Exists("Intentos.dat"))
+            if (ComprobarArchivo(3))
             {
-                flujo = new FileStream("Intentos.dat", FileMode.Append, FileAccess.Write);
+                flujo = new FileStream(archivo_intentos, FileMode.Append, FileAccess.Write);
             }
             else
             {
-                flujo = new FileStream("Intentos.dat", FileMode.Create, FileAccess.Write);
+                flujo = new FileStream(archivo_intentos, FileMode.Create, FileAccess.Write);
             }
             serie.Serialize(flujo, error);
             flujo.Close();
@@ -130,11 +171,11 @@ namespace SMS_Collector
                     flujo2.Close();
                     if (archivoorigen.CompareTo("Datos.dat") == 0)
                     {
-                        MessageBox.Show("Copia de Seguridad realizada con éxito", "ATENCIÓN", MessageBoxButtons.OK);
+                        MessageBox.Show("Copia de Seguridad realizada con éxito", "ATENCIÓN", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     }
                     else
                     {
-                        MessageBox.Show("Copia de Seguridad restaurada con éxito", "ATENCIÓN", MessageBoxButtons.OK);
+                        MessageBox.Show("Copia de Seguridad restaurada con éxito", "ATENCIÓN", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     }
                 }
             }
@@ -142,11 +183,11 @@ namespace SMS_Collector
             {
                 if (archivoorigen.CompareTo("Datos.dat") == 0)
                 {
-                    MessageBox.Show("No existe ningun fichero de datos", "ATENCIÓN", MessageBoxButtons.OK);
+                    MessageBox.Show("No existe ningun fichero de datos", "ATENCIÓN", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 }
                 else
                 {
-                    MessageBox.Show("No existe ninguna Copia de Seguridad para restaurar", "ATENCIÓN", MessageBoxButtons.OK);
+                    MessageBox.Show("No existe ninguna Copia de Seguridad para restaurar", "ATENCIÓN", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
             }
         }
@@ -158,9 +199,9 @@ namespace SMS_Collector
             SMS datos2;
             int sw;
 
-            if (File.Exists("Datos.dat"))
+            if (ComprobarArchivo(4))
             {
-                flujo = new FileStream("Datos.dat", FileMode.Open, FileAccess.Read);
+                flujo = new FileStream(archivo_datos, FileMode.Open, FileAccess.Read);
                 try
                 {
                     while (true)
@@ -226,18 +267,18 @@ namespace SMS_Collector
                         }
                     } while (sw == 1);
                     CrearArchivo(coleccion);
-                    MessageBox.Show("Registro ordenado con éxito", "Información", MessageBoxButtons.OK);
+                    MessageBox.Show("Registro ordenado con éxito", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
             }
             else
             {
-                MessageBox.Show("No existe ningún registro", "Atención", MessageBoxButtons.OK);
+                MessageBox.Show("No existe ningún registro", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
 
         public void CrearArchivo(ArrayList coleccion)
         {
-            flujo = new FileStream("Datos.dat", FileMode.Create, FileAccess.Write);
+            flujo = new FileStream(archivo_datos, FileMode.Create, FileAccess.Write);
             for (int i = 0; i < coleccion.Count; i++)
             {
                 serie.Serialize(flujo, (SMS)coleccion[i]);
@@ -247,13 +288,13 @@ namespace SMS_Collector
 
         public void AnadirSMS(SMS mensaje)
         {
-            if (File.Exists("Datos.dat"))
+            if (ComprobarArchivo(4))
             {
-                flujo = new FileStream("Datos.dat", FileMode.Append, FileAccess.Write);
+                flujo = new FileStream(archivo_datos, FileMode.Append, FileAccess.Write);
             }
             else
             {
-                flujo = new FileStream("Datos.dat", FileMode.Create, FileAccess.Write);
+                flujo = new FileStream(archivo_datos, FileMode.Create, FileAccess.Write);
             }
             serie.Serialize(flujo, mensaje);
             flujo.Close();
@@ -264,9 +305,9 @@ namespace SMS_Collector
             ArrayList coleccion = new ArrayList();
             SMS datos;
 
-            if (File.Exists("Datos.dat"))
+            if (ComprobarArchivo(4))
             {
-                flujo = new FileStream("Datos.dat", FileMode.Open, FileAccess.Read);
+                flujo = new FileStream(archivo_datos, FileMode.Open, FileAccess.Read);
                 try
                 {
                     while (true)
@@ -296,7 +337,7 @@ namespace SMS_Collector
             }
             else
             {
-                MessageBox.Show("No existe ningún registro", "Atención", MessageBoxButtons.OK);
+                MessageBox.Show("No existe ningún registro", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
             return coleccion;
         }
@@ -306,9 +347,9 @@ namespace SMS_Collector
             ArrayList coleccion = new ArrayList();
             SMS datos;
 
-            if (File.Exists("Datos.dat"))
+            if (ComprobarArchivo(4))
             {
-                flujo = new FileStream("Datos.dat", FileMode.Open, FileAccess.Read);
+                flujo = new FileStream(archivo_datos, FileMode.Open, FileAccess.Read);
                 try
                 {
                     while (true)
@@ -329,7 +370,7 @@ namespace SMS_Collector
             }
             else
             {
-                MessageBox.Show("El Historial esta vacío", "Atención", MessageBoxButtons.OK);
+                MessageBox.Show("El Historial esta vacío", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
 
             return coleccion;
@@ -339,9 +380,9 @@ namespace SMS_Collector
         {
             bool encontrado = false;
 
-            if (File.Exists("Numeros.dat"))
+            if (ComprobarArchivo(2))
             {
-                FileStream flujo = new FileStream("Numeros.dat", FileMode.Open, FileAccess.Read);
+                FileStream flujo = new FileStream(archivo_numeros, FileMode.Open, FileAccess.Read);
                 try
                 {
                     while (true)
